81f2b47f8757a10037257dfffa5401371984cdc1
Daniel Tea <daniel_tea@spe.sony.com>
2016-12-03 17:35:31 -0800

    Adds basic components

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+// Components
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+
+
+        <Order />
+
+
+        <Inventory />
+      </div>
+    );
+  };
+}
+
+export default App;
+
