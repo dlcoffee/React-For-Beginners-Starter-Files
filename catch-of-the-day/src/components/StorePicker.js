8eb3ca4742302389e038ee60c1449111620c5b07
Daniel Tea <daniel_tea@spe.sony.com>
2016-12-03 17:35:31 -0800

    Adds basic components

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class StorePicker extends React.Component {
+
+  render() {
+    return (
+      <form className="store-selector">
+        { /* a comment */ }
+        <h2>Please Enter A Store</h2>
+
+        <input type="text" required placeholder="Store Name"/>
+        <button type="submit">Vist Store </button>
+      </form>
+    );
+  }
+
+}
+
+export default StorePicker;
+
