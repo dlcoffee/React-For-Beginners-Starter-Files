a9bbdd05a7d4beef275df98c38fce30e1f5f3528
Daniel Tea <daniel_tea@spe.sony.com>
2016-12-24 15:45:44 -0800

    animations added

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import {formatPrice} from '../helpers';
 
+import CSSTransitionGroup from 'react-addons-css-transition-group';
+
 class Order extends React.Component {
   constructor() {
     super()
@@ -24,9 +26,19 @@ class Order extends React.Component {
 
     return (
       <li key={key}>
-        <span>{count}lbs {fish.name}</span>
+        <span>
+          <CSSTransitionGroup
+            component="span"
+            className="count"
+            transitionName="count"
+            transitionEnterTimeout={250}
+            transitionLeaveTimeout={250}
+          >
+            <span key={count}>{count}</span>
+          </CSSTransitionGroup>
+          lbs {fish.name} {removeButton}
+        </span>
         <span className="price">{formatPrice(count * fish.price)}</span>
-        {removeButton}
       </li>
     )
   }
@@ -49,14 +61,22 @@ class Order extends React.Component {
     return (
       <div className="order-wrap">
         <h2>Your Order</h2>
-        <ul className="order">
+
+        <CSSTransitionGroup
+          component="ul"
+          transitionName="order"
+          className="order"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}
+        >
           {orderIds.map(this.renderOrder)}
 
           <li className="total">
             <strong>Total:</strong>
             {formatPrice(total)}
           </li>
-        </ul>
+        </CSSTransitionGroup>
+
       </div>
     );
   };
